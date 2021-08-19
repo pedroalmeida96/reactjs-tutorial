ad93c21418c478b615577f7ba458e2e303cfab27
pedroalmeida96 <pedro.gregorio96@gmail.com>
2021-08-19 21:08:44 +0100

    onClick

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,26 @@
 const Home = () => {
   const firstTry = "This is my first try with react.";
+
+  const clickMeHandler = () => {
+    console.log("Button clicked");
+  };
+
+  const clickMeTooHandler = (name, evt) => {
+    console.log("Hello " + name, evt.target);
+  };
+
   return (
     <div className="home">
       <h2>HOMEPAGE</h2>
       <p>{firstTry}</p>
+      <button onClick={clickMeHandler}>Click Me</button>
+      <button
+        onClick={(evt) => {
+          clickMeTooHandler("Pedro", evt);
+        }}
+      >
+        Click Me Too
+      </button>
     </div>
   );
 };
